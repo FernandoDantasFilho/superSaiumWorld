cdb807c236f219eabc1ff208219034886cb06b6b
DESKTOP-B2NHE2D\fernandodantas <fernandoalbino755@gmail.com>
2016-11-30 23:47:27 -0400

    site

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1480563931,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/logo-sheet0.png",
+		"images/bgnear.png",
+		"images/bgdistant.png",
+		"images/ground.png",
+		"images/pedra-sheet0.png",
+		"images/tronco-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/groundsolid-sheet0.png",
+		"images/cactus-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
